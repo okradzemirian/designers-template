35220560c269e39d0f342aa52e45d521c18e84a5
okradzemirian <okradzemirian@gmail.com>
2019-09-13 00:07:53 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import 'intersection-observer'
+import 'normalize.css'
+import './scss/index.scss'
+
+const $navToggleButton = document.getElementById('nav-toggle-button')
+const $navList = document.getElementById('nav-toggle')
+const $navItems = document.querySelectorAll('.nav__link')
+
+// nav toggle on mobile
+
+const toggleNavOpen = () => {
+    $navList.classList.toggle('nav__list--open')
+    document
+        .querySelector('.nav-toggle-button')
+        .classList.toggle('nav-toggle-button--open')
+}
+
+$navToggleButton.addEventListener('click', () => {
+    toggleNavOpen()
+})
+
+$navItems.forEach($navItem => {
+    $navItem.addEventListener('click', toggleNavOpen)
+})
+
+// change nav background on scroll
+
+const $header = document.querySelector('.header')
+const $homeSection = document.getElementById('home')
+
+const aboutSectionObserver = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                $header.classList.add('header-scrolled')
+            } else {
+                $header.classList.remove('header-scrolled')
+            }
+        })
+    },
+    {
+        rootMargin: '-300px 0px 0px 0px',
+    },
+)
+
+aboutSectionObserver.observe($homeSection)
